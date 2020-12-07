77008565e763e1aebeee926055108842beca6c13
JasonI136 <63634201+JasonI136@users.noreply.github.com>
2020-12-07 18:38:38 +1100

    Working Build

---
+++ Building-Website-Backend/Palisade.Shared/Helper/BuildingWebsiteConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Palisade.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palisade.Shared.Helper
+{
+    public sealed class BuildingWebsiteConfiguration
+    {
+        private static BuildingWebsiteConfiguration instance = null;
+        private static readonly object padlock = new object();
+        public IConfiguration Configuration { get; set; }
+        BuildingWebsiteConfiguration()
+        { 
+        }
+
+        public static BuildingWebsiteConfiguration Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (padlock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new BuildingWebsiteConfiguration();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+    }
+}
